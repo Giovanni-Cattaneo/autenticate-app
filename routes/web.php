5798057da16b2a3c1ef55944b589119713a61615
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-20 11:04:24 +0200

    edit layouts to show dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +26,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::middleware('auth', 'verified')
+    ->name('adimn.')
+    ->prefix('admin')
+    ->group(function () {
+        // metti qui le rotte protette da autenticazione di sistema
+        // tutte le rotte devono condividere stesso nome e prefix  e il middleware
+        // il nome sarà una roba tipo admin. cosi da concatenare altre rotte tutte con inizio adimn
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); // name sarà admin.dashboard
+    });
